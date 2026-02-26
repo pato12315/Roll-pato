872fd00d76851936964d58ffdd7c518f698f42ad
pato12315 <nozato.daiki@gmail.com>
2026-02-26 15:15:15 +0900

    Create index.js as the main entry point for the Duck RPG game

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Main entry point for the Duck RPG Game
+class DuckRPG {
+    constructor() {
+        console.log('Welcome to Duck RPG!');
+    }
+
+    start() {
+        console.log('Game is starting...');
+    }
+}
+
+const game = new DuckRPG();
+game.start();
